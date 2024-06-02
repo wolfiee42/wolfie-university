68330a9333e1554420230a8cb7ccc6a06db69925
wolfiee42 <saifalislam2022@gmail.com>
2024-06-02 20:35:15 +0600

    underatanding the concept of Transaction & Rollback, and implementing it in the code

--- backend/src/app/modules/user/06.user.service.ts
+++ backend/src/app/modules/user/06.user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import configaration from "../../configaration";
 import { academicModel } from "../academicSemester/02.academicSemester.model";
 import { TStudent } from "../student/01.student.interface";
@@ -5,6 +6,8 @@ import { studentModel } from "../student/02.student.model";
 import { TUser } from "./01.user.interface";
 import { userModel } from "./02.user.model";
 import { generateStudentID } from "./07.user.utils";
+import AppError from "../../errors/AppError";
+import httpStatus from "http-status";
 
 // create student
 const createStudentToDB = async (password: string, studentInfo: TStudent) => {
@@ -20,28 +23,52 @@ const createStudentToDB = async (password: string, studentInfo: TStudent) => {
     // role define
     userData.role = 'student';
 
-    // user id
-    if (admissionSemester) {
-        userData.id = await generateStudentID(admissionSemester);
-    } else {
-        console.warn("Admission semester not available for student ID generation.");   // Handle the case where admissionSemester is null (e.g., log a warning)
-    }
+    // Transaction & Rollback
+    const session = await mongoose.startSession();
 
-    // create a user
-    const newUser = await userModel.create(userData);
+    try {
+        // startting the session
+        session.startTransaction();
 
-    // create a student
-    if (Object.keys(newUser).length) {
+        // user id
+        if (admissionSemester) {
+            userData.id = await generateStudentID(admissionSemester);
+        } else {
+            console.warn("Admission semester not available for student ID generation.");   // Handle the case where admissionSemester is null (e.g., log a warning)
+        }
 
+        // create a user  // transaction-01
+        const newUser = await userModel.create([userData], { session });
+
+        // create a student
+        if (!newUser.length) {
+            throw new AppError(httpStatus.BAD_REQUEST, 'Failed to Create User.')
+        }
         // set id, _id as user
-        studentInfo.id = newUser.id;
-        studentInfo.user = newUser._id;
+        studentInfo.id = newUser[0].id;
+        studentInfo.user = newUser[0]._id;
 
-        const newStudent = await studentModel.create(studentInfo);
+
+        // create a student  // transaction-02
+        const newStudent = await studentModel.create([studentInfo], { session });
+
+        if (!newStudent.length) {
+            throw new AppError(httpStatus.BAD_REQUEST, 'Failed to Create Student.')
+        }
+
+        await session.commitTransaction();
+        await session.endSession();
+
         return newStudent;
+
+    } catch (error) {
+        await session.abortTransaction();
+        await session.endSession();
     }
 
-    return newUser;
+
+
+
 }
 
 export const userServices = {
